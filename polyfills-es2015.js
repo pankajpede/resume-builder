b923605f53aa19b63856e86c7c4ffaa7ffff7fe0
Pankaj Pede <pede.pankaj@gmail.com>
2020-08-22 20:41:19 +0530

    design for mobile added

--- polyfills-es2015.js
+++ polyfills-es2015.js
@@ -6354,8 +6354,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Pankaj\Data\Resume-builder\angular-8-export-pdf-master\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Pankaj\Data\Resume-builder\angular-8-export-pdf-master\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /Users/pankajpede/Documents/Project/Resume Builder/resume-builder/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/pankajpede/Documents/Project/Resume Builder/resume-builder/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
--- polyfills-es5.js
+++ polyfills-es5.js
@@ -13289,10 +13289,10 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Pankaj\Data\Resume-builder\angular-8-export-pdf-master\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
-__webpack_require__(/*! C:\Pankaj\Data\Resume-builder\angular-8-export-pdf-master\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
-__webpack_require__(/*! C:\Pankaj\Data\Resume-builder\angular-8-export-pdf-master\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Pankaj\Data\Resume-builder\angular-8-export-pdf-master\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /Users/pankajpede/Documents/Project/Resume Builder/resume-builder/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
+__webpack_require__(/*! /Users/pankajpede/Documents/Project/Resume Builder/resume-builder/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
+__webpack_require__(/*! /Users/pankajpede/Documents/Project/Resume Builder/resume-builder/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/pankajpede/Documents/Project/Resume Builder/resume-builder/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
